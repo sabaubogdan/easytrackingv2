20debbb129b38b274274248165261841af60c49f
Bogdan Sabau <sabaubogdanvasile@gmail.com>
2018-02-26 20:33:10 +0200

    first commit

---
+++ src/main/java/xyz/vegaone/easytrackingv2/Easytrackingv2Application.java
@@ -0,0 +1,12 @@
+package xyz.vegaone.easytrackingv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Easytrackingv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Easytrackingv2Application.class, args);
+	}
+}
